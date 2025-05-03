1b17dbc6cc75bf71304c8f39698adb82d76f1853
Massimo Nocentini <massimo.nocentini@gmail.com>
2025-05-03 17:41:02 +0200

    initial code to setup a link

---
+++ src/test/test.scm
@@ -0,0 +1,9 @@
+
+(import aux wolfram)
+
+(let1 (env/pointer (make-env))
+(display env/pointer)
+(define link/pointer (openstring env/pointer))
+(display link/pointer)
+)
+(display 'end)
--- src/wolfram.scm
+++ src/wolfram.scm
@@ -1,5 +1,34 @@
 
 (module wolfram
-	*
+  *
+  (import scheme (chicken base) (chicken foreign) (chicken gc) aux)
 
-	)
+  (foreign-declare "#include <wstp.h>")
+
+  (define WSEOK (foreign-value "WSEOK" int))
+
+  (define WSInitialize (foreign-lambda c-pointer "WSInitialize" c-pointer))
+  (define WSDeinitialize (foreign-lambda void "WSDeinitialize" c-pointer))
+  (define WSOpenString (foreign-lambda c-pointer "WSOpenString" c-pointer c-string (c-pointer int)))
+  (define WSClose (foreign-lambda void "WSClose" c-pointer))
+  (define WSActivate (foreign-lambda int "WSActivate" c-pointer))
+
+  (define (make-env)
+    (let1 (handle (WSInitialize #f))
+          (set-finalizer! handle WSDeinitialize)
+          handle))
+
+  (define (openstring env/pointer)
+    (let-location ((i int))
+                  (let1 (p (WSOpenString env/pointer
+                                         "csi -linkmode connect -linkname 8081 -linkprotocol TCPIP -linkoptions 4"
+                                         (location i)))
+                        (unless (equal? WSEOK i) (error `(WSOpenString ,p)))
+                        (set-finalizer! p WSClose)
+                        (when (zero? (WSActivate p)) (error `(WSActivate ,p)))
+                        p)))
+
+
+
+  )
+
