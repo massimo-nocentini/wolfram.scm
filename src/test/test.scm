ff16072a2fb61386b4aa6c72f7076a41a05c9e81
Massimo Nocentini <massimo.nocentini@gmail.com>
2025-05-04 13:41:01 +0200

    First working version

--- src/test/test.scm
+++ src/test/test.scm
@@ -5,6 +5,7 @@
 (display env/pointer)
 (define link/pointer (openstring env/pointer))
 (display link/pointer)
-(display ((evaluate link/pointer) '(Times 3 4)))
+(display ((evaluate link/pointer) '(Plus x (Plus x x) x) #;(Times 3 4)))
+(display ((evaluate link/pointer) '(ToExpression  "3+4/Sqrt[19.2]")))
 )
 (display 'end)
