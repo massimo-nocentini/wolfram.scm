483b942ccf6c6aceced0bb13e7a29ecca8ae43e3
Massimo Nocentini <massimo.nocentini@gmail.com>
2025-11-03 17:34:12 +0100

    A change for graphs

--- src/test/hansei-graph.scm
+++ src/test/hansei-graph.scm
@@ -40,40 +40,18 @@
   
    (define result 
      (probcc-reify/exact
-       (let* ((max-t 10))
-          (let R ((t 0) 
-                  (v_i (probcc-distribution/wolfram 'w '((Subscript a 0) (Subscript b 0) (Subscript c 0))))
-                  (track '()))
-            (cond
-              ((= t max-t) (reverse track))
-              (else (let* ((v_j (probcc-distribution/wolfram 'w (letassoc/cdr (v_i tgraph) (else '())))))
-                        (R (+ t 1) v_j (cons (list v_i v_j t) track)))))))))
+       (let* ((max-t 10)
+              (v_i (probcc-distribution/wolfram 'w '((Subscript a 0) (Subscript b 0) (Subscript c 0)))))
+          (let R ((v_i v_i)
+                  (track (list v_i)))
+            (let1 (t (caddr v_i))
+              (cond
+                ((>= t max-t) (cons 'List (reverse (cons v_i track))))
+                (else (let* ((v_j (probcc-distribution/wolfram 'w (cons `(Subscript ,(cadr v_i) ,(add1 t)) (letassoc/cdr (v_i tgraph) (else '()))))))
+                          (R v_j (if (equal? (cadr v_i) (cadr v_j)) track (cons v_j track)))))))))))
 
    (define normalized (probcc-normalize result))
    (define result/W (map (λ (each) (match each (`((V ,lst) ,p) `(Rule ,lst ,p)))) normalized))
-
-   #;(⊦= '(((V ((rain #t) (sprinkler #t) (grass-is-wet #t)))
-            (Times r
-                   s
-                   (Plus v
-                         (Times e (Plus -1 v) (Plus -1 w))
-                         w
-                         (Times -1 v w))))
-           ((V ((rain #t) (sprinkler #f) (grass-is-wet #t)))
-            (Times r (Plus -1 s) (Plus (Times e (Plus -1 w)) (Times -1 w))))
-           ((V ((rain #f) (sprinkler #t) (grass-is-wet #t)))
-            (Times (Plus -1 r) s (Plus (Times e (Plus -1 v)) (Times -1 v))))
-           ((V ((rain #t) (sprinkler #t) (grass-is-wet #f)))
-            (Times -1 (Plus -1 e) r s (Plus -1 v) (Plus -1 w)))
-           ((V ((rain #t) (sprinkler #f) (grass-is-wet #f)))
-            (Times -1 (Plus -1 e) r (Plus -1 s) (Plus -1 w)))
-           ((V ((rain #f) (sprinkler #t) (grass-is-wet #f)))
-            (Times -1 (Plus -1 e) (Plus -1 r) s (Plus -1 v)))
-           ((V ((rain #f) (sprinkler #f) (grass-is-wet #f)))
-            (Times -1 (Plus -1 e) (Plus -1 r) (Plus -1 s)))
-           ((V ((rain #f) (sprinkler #f) (grass-is-wet #t)))
-            (Times e (Plus -1 r) (Plus -1 s))))
-         result)
 
    `(doc (p "If we remove the " (i "observation") (code/scheme `(first ,(caar normalized)))
             "from the previous test, then we can show the " (i "joint distribution"))
